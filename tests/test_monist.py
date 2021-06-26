a2dc31ba4e0ec9ea097658e88b6f3ce02408cf4a
Brendan Strejcek <brendan@datagazing.com>
2021-06-25 23:16:31 -0400

    initial commit

---
+++ tests/test_monist.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `monist` package."""
+
+
+import unittest
+
+from monist import monist
+
+
+class TestMonist(unittest.TestCase):
+    """Tests for `monist` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
